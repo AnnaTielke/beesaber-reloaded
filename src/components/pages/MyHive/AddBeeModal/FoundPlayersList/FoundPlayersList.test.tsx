3e9c34b9dc580617582ed2092ec72c16edac8986
André Sebastian-Bath <a.sebastian@posteo.de>
2022-01-30 20:43:50 +0100

    add test todos

---
+++ src/components/pages/MyHive/AddBeeModal/FoundPlayersList/FoundPlayersList.test.tsx
@@ -0,0 +1,7 @@
+describe('FoundPlayersList', () => {
+    it.todo('should match snapshot for multiple found players');
+    it.todo('should match snapshot for one found player');
+    it.todo('should not render, when no players are found or userdata is undefined');
+    it.todo('should mark found player correctly as already added');
+    it.todo('should mark correctly as only one user found');
+});
---
+++ src/components/pages/MyHive/AddBeeModal/FoundPlayersList/OnePlayer/OnePlayer.test.tsx
@@ -0,0 +1,9 @@
+describe('OnePlayer', () => {
+    it.todo('should match snapshot for multiple found players');
+    it.todo('should match snapshot for one found player');
+    it.todo('should show plus icon, when multiple player are found');
+    it.todo('should show minus icon, when only one player found');
+    it.todo('should hide icon, when player is already added');
+    it.todo('should show avataer playholder, when no avatar presend / steam or oculus symbol');
+    it.todo('should call handleSelect, when icon clicked');
+});
